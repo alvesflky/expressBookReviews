d3018a8f2a7d4b3263c662e1f926a874e08e0ade
alvesflky <alvesfl@kyndryl.com>
2025-10-09 12:20:29 -0300

    Update index.js

--- final_project/index.js
+++ final_project/index.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 const session = require('express-session')
 const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
-
+const PORT =5000;
 const app = express();
 
 app.use(express.json());
@@ -28,7 +28,7 @@ app.use("/customer/auth/*", function auth(req,res,next){
         return res.status(403).json({ message: "User not logged in" });
 }});
  
-const PORT =5000;
+
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
